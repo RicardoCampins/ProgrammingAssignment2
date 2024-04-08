545b0402ab515479677ae1b43c92cf38c913d930
RicardoCampins <ricardo.campins@gmail.com>
2024-04-08 10:17:02 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function reads the matrix and sets the setter and getter functions
+## for both the original matrix (M) and the inverse matrix (MI). It does not 
+## work by itself but in conjuction with the following function.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(M = matrix()) {
+    MI <- NULL
+    set <- function(y) {
+        M <<- y
+        MI <<- NULL
+    }
+    get <- function() M
+    setinv <- function(solve) MI <<- solve
+    getinv <- function() MI
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function reads the original matrix (M), checks whether the inverted 
+## matrix (MI) remains as a null object or if a value has been assigned to it.
+## Reads the matrix data and uses the solve function to calculate and return 
+## the inverse matrix.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(M, ...) {
+    ## Return a matrix that is the inverse of 'M'
+    MI <- M$getinv()
+    if(!is.null(MI)) {
+        message("getting cached data")
+        return(MI)
+    }
+    data <- M$get()
+    MI <- solve(data, ...)
+    M$setinv(MI)
+    MI
 }
+
+
+## Example
+
+a1 <- c(3, 2, 5)
+a2 <- c(2, 3, 2) 
+a3 <- c(5, 2, 4)
+A <- rbind(a1, a2, a3)
+
+cacheSolve(makeCacheMatrix(A))
+
